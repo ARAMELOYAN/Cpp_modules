6f5c4e3ac0059115326333ca4d6d12c5a068a7c7
Aram Eloyan <aeloyan@c2r4s3.42yerevan.am>
2023-07-06 14:43:58 +0400

    sdf

--- module_00/ex01/phonebook.hpp
+++ module_00/ex01/phonebook.hpp
@@ -8,9 +8,12 @@ class PhoneBook {
 		std::string f_name[8];
 		std::string l_name[8];
 		std::string n_name[8];
+		std::string phone_number[8];
+		std::string darkness_secret[8];
 	public:
 		PhoneBook();
-		void	add(std::string fname, std::string lname, std::string nname);
+		void	add(std::string fname, std::string lname, std::string nname,
+				std::string phone_number, std::string darkness_secret);
 		void	search();
 }
 
