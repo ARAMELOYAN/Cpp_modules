926b5f0bc318f8f8313e13116a708ce20824ca62
Aram Eloyan <aeloyan@c2r9s1.42yerevan.am>
2023-07-20 12:32:37 +0400

    mesig

---
+++ module_01/ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int	main(void)
+{
+	std::string brainz("HI THIS IS BRAIN");
+	std::string* stringPTR = &brainz;
+	std::string& stringREF = brainz;
+
+	std::cout << &brainz << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+	
+	std::cout << brainz << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+	return 0;
+}
