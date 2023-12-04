16be9092b88f0f942aa2c8961eb6d648f15a3169
voskecity <voskecity@github.com>
2023-12-04 14:03:15 +0400

    sad

---
+++ module_09/ex02/PmergeMe.cpp
@@ -0,0 +1,13 @@
+#include "PmergeMe.hpp"
+#include <string>
+
+PmergeMe::PmergeMe(std::vector<int> vec)
+{
+	
+	std::cout << GREEN "PmergeMe parametric constructor" RESET "\n";
+}
+
+PmergeMe::~PmergeMe()
+{
+	std::cout << GREEN "PmergeMe destructor" RESET "\n";
+}
---
+++ module_09/ex02/PmergeMe.hpp
@@ -0,0 +1,11 @@
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+# include <iostream>
+
+class PmergeMe
+{
+		Pmergeme(const Pmergeme& copy);
+		PmergeMe& Pmergeme(const Pmergeme& copy);
+	public:
+		Pmergeme();
+		~Pmergeme();
